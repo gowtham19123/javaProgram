4deeddb8f9b8339076afa531c9f86b7e6f7c05cd
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-07-24 08:33:09 +0530

    Update Shopping_bill.java

--- Assignment-2/Shopping_bill.java
+++ Assignment-2/Shopping_bill.java
@@ -5,11 +5,11 @@ public class Shopping_bill{
 public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
       System.out.println("Product A Price: ");
-      double product_A = sc.nextInt();
+      double product_A = sc.nextDouble();
       System.out.println("Product B Price: ");
-      double product_B = sc.nextInt();
+      double product_B = sc.nextDouble();
       System.out.println("Product C Price: ");
-      double product_C = sc.nextInt();
+      double product_C = sc.nextDouble();
       double Total_price = product_A + product_B +product_C;
       System.out.println("Total_ammount = "+Total_price);
       double Discount = Total_price * 0.10;
