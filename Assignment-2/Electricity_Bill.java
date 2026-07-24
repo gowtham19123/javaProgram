ba1f0af431feeb52b495880ee921301753e635eb
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-07-24 08:32:00 +0530

    Update Electricity_Bill.java

--- Assignment-2/Electricity_Bill.java
+++ Assignment-2/Electricity_Bill.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
 
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter a total units");
-         float units = sc.nextInt();
+         float units = sc.nextIFloat();
          double charge = units * 8.50;
          double e_tax = charge * 0.05;
          double final_bill = charge + 150 + e_tax; 
